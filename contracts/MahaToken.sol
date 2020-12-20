50d4b826d3975ee8d13303faf980cce5059c4fb3
Steven Enamakel <enamakel@vezures.com>
2020-12-20 10:51:28 +0530

    update vesting contracts

---
+++ contracts/MahaToken.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.6.0;
+
+import "../node_modules/@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
+
+contract MahaToken is ERC20PresetMinterPauser {
+    address public upgradedAddress;
+    bool public deprecated;
+    string public contactInformation = "contact@mahadao.com";
+    string public reason;
+    string public link = "https://mahadao.com";
+    string public url = "https://mahadao.com";
+    string public website = "https://mahadao.io";
+
+    constructor() public ERC20PresetMinterPauser("MahaDAO", "MAHA") {}
+}
